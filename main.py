5d6396393dddc11ac0cb54eb129500f17e18214e
Jamshid <urologiya@internet.ru>
2022-07-12 08:22:43 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,70 @@
+import requests
+from twilio.rest import Client
+
+STOCK_NAME = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+STOCK_API_KEY = "2B0QTC6VA9YJ3QMM"
+NEWS_API_KEY = "0e24065c7f66480f8a56d614e2216823"
+TWILIO_SID = "ACdb05c9a4fa256984a8c3165ac624d74f"
+TWILIO_AUTH_TOKEN = "d25233b54419ec514417deb0ed9c96cc"
+
+stock_params = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK_NAME,
+    "apikey": STOCK_API_KEY,
+}
+
+response = requests.get(STOCK_ENDPOINT, params=stock_params)
+response.raise_for_status()
+data = response.json()["Time Series (Daily)"]
+data_list = [value for (key, value) in data.items()]
+yesterday_data = data_list[0]
+yesterday_closing_price = yesterday_data["4. close"]
+
+day_before_yesterday_data = data_list[1]
+day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
+
+difference = float(yesterday_closing_price) - float(day_before_yesterday_closing_price)
+up_down = None
+if difference > 0:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
+diff_percent = round((difference / float(yesterday_closing_price)) * 100)
+
+if abs(diff_percent) > 5:
+    news_params = {
+        "apiKey": NEWS_API_KEY,
+        "qInTitle": COMPANY_NAME,
+    }
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    news_response.raise_for_status()
+    articles = news_response.json()["articles"]
+    three_articles = articles[:3]
+
+    formatted_articles = [
+        f"{STOCK_NAME}: {up_down}{diff_percent}%\nHeadline: {article['title']}. \nBrief: {article['description']}" for
+        article in three_articles]
+
+    client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    for article in formatted_articles:
+        message = client.messages \
+            .create(
+            body=article,
+            from_="+18508015279",
+            to='+998990295433',
+        )
+
+# Optional: Format the SMS message like this:
+"""TESLA: 🔺2% Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TESLA)?. Brief: We at Insider Monkey have 
+gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings 
+show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash. 
+or "TESLA: 🔻5% Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TESLA)?. Brief: We at Insider Monkey 
+have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F 
+filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus 
+market crash. """
